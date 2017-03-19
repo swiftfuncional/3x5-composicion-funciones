02e5d4f515d51560ae933e3c40c7dc8365f8f5a3
Swift Funcional <info@swiftfuncional.com>
2017-03-19 11:34:39 +0100

    Format prices method (imperative way)

---
+++ Composicion de funciones.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+func formatPrices(json: String) -> [String] {
+	let prices = try! JSONSerialization.jsonObject(with: json.data(using: .utf8)!, options: []) as! [AnyObject]
+
+	var labels = [String]()
+
+	for p in prices {
+		if let price = p as? NSNumber {
+			let label: String
+
+			if price == 0 {
+				label = "Free"
+			} else {
+				let formatter = NumberFormatter()
+				formatter.numberStyle = .currency
+				formatter.locale = Locale(identifier: "es_ES")
+
+				label = formatter.string(from: price)!
+			}
+
+			labels.append(label)
+		}
+	}
+
+	return labels
+}
